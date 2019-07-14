6ec18793e9204394796cb9d4322e97a53421838f
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2019-07-14 12:12:32 +0530

    Added Button Component and Pack Command

---
+++ projects/button/src/lib/button.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'bb-button',
+  template: `
+    <button [disabled]="disabled">
+      {{text}}
+    </button>
+  `,
+  styles: []
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string;
+  @Input() disabled: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
